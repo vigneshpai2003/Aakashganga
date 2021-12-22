b278b065e0f1692d877b305849132ca40372de32
ViggyPiggy <57759856+vigneshpai2003@users.noreply.github.com>
2021-12-22 12:30:25 +0530

    added colorlib template

--- aakashganga/settings.py
+++ aakashganga/settings.py
@@ -54,7 +54,10 @@ ROOT_URLCONF = 'aakashganga.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "templates",
+            BASE_DIR / "agApp/templates",
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static/'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
