5b8ca375add905a4ec0e6cd0b14de2270d2fc4f6
ViggyPiggy <57759856+vigneshpai2003@users.noreply.github.com>
2021-12-30 21:51:15 +0530

    working nav bar

---
+++ static/js/nav.js
@@ -0,0 +1,18 @@
+function main() {
+    const menu_toggle = document.querySelector('.menu-toggle')
+    const nav = document.querySelector('.nav-link')
+    const nav_links = document.querySelectorAll('.nav-link li')
+
+    menu_toggle.addEventListener('click', function () {
+        nav.classList.toggle('menu-active')
+
+        nav_links.forEach(function (link, index) {
+            if (link.style.animation)
+                link.style.animation = ''
+            else
+                link.style.animation = 'menuFade 0.5s ease forwards ' +  (index / 10 + .5) + 's'
+        })
+    })
+}
+
+main()
