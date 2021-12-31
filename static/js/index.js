bd4105529be2706854fa0adf9db2dca630899d1c
ViggyPiggy <57759856+vigneshpai2003@users.noreply.github.com>
2021-12-31 22:21:24 +0530

    fading, better footer, link on title

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,15 @@
 window.addEventListener('scroll', function () {
     document.querySelector('.background').style.backgroundPosition = "center " + document.scrollingElement.scrollTop * 0.5 + "px"
 })
+
+$(window).scroll(function () {
+    const scrollTop = $(this).scrollTop()
+
+    $('.fade').each(function () {
+        $(this).css({
+            opacity: function () {
+                return (scrollTop / $(this).height()) ** 3
+            }
+        })
+    })
+});
