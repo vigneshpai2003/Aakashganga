8d12bee69c7d5fd0c3d05c7e6ffae5dbf8848e10
Vignesh <vigneshpai2003@gmail.com>
2022-01-08 00:00:34 +0530

    dummy pages

--- AGapp/routes.py
+++ AGapp/routes.py
@@ -9,3 +9,23 @@ def index():
 @app.route('/team')
 def team():
 	return render_template('team.html')
+
+@app.route('/aakashvani')
+def aakashvani():
+	return render_template('aakashvani.html')
+
+@app.route('/events')
+def events():
+	return render_template('events.html')
+
+@app.route('/gallery')
+def gallery():
+	return render_template('gallery.html')
+
+@app.route('/facilities')
+def facilties():
+	return render_template('facilities.html')
+
+@app.route('/dhruv')
+def dhruv():
+	return render_template('dhruv.html')
