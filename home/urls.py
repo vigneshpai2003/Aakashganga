04d7a95e1f6de315cf383bc53082b18e55e9d0bb
Vignesh <vigneshpai2003@gmail.com>
2022-01-13 23:04:28 +0530

    all features ported except aakashvani

---
+++ home/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('team', views.team, name="team"),
+    path('aakashvani', views.aakashvani, name="aakashvani"),
+    path('events', views.events, name="events"),
+    path('gallery', views.gallery, name="gallery"),
+    path('facilities', views.facilities, name="facilities"),
+    path('dhruv', views.dhruv, name="dhruv"),
+]
---
+++ home/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect
+
+from .models import Announcement, Event
+
+
+def index(request):
+    return render(request, "home/index.html", {
+        'announcement': Announcement.objects.first(),
+        'events': sorted(Event.objects.all().live(), key=lambda x: x.priority)
+    })
+
+def team(request):
+    return render(request, "home/team.html")
+
+def aakashvani(request):
+    return render(request, "home/aakashvani.html")
+
+def events(request):
+    return render(request, "home/events.html")
+
+def gallery(request):
+    return render(request, "home/gallery.html")
+
+def facilities(request):
+    return render(request, "home/facilities.html")
+
+def dhruv(request):
+    return render(request, "home/dhruv.html")
