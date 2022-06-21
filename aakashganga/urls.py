dc5b76ebb605f422c32646284f6983f3099fc903
Vignesh <vigneshpai2003@gmail.com>
2022-06-21 19:10:02 +0530

    reset code, added media files from server

--- aakashganga/urls.py
+++ aakashganga/urls.py
@@ -1,8 +1,6 @@
 from django.conf import settings
 from django.urls import include, path
 from django.contrib import admin
-from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
@@ -22,6 +20,9 @@ urlpatterns = [
 
 
 if settings.DEBUG:
+    from django.conf.urls.static import static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
@@ -36,6 +37,3 @@ urlpatterns = urlpatterns + [
     # of your site, rather than the site root:
     #    path("pages/", include(wagtail_urls)),
 ]
-
-urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
