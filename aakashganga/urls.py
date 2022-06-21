9d7325f97966f5711a30cec470ce8041f213db52
Vignesh <vigneshpai2003@gmail.com>
2022-06-21 18:40:53 +0530

    import error

--- aakashganga/urls.py
+++ aakashganga/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.urls import include, path
 from django.contrib import admin
+from django.conf.urls.static import static
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
@@ -20,7 +21,6 @@ urlpatterns = [
 
 
 if settings.DEBUG:
-    from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
     # Serve static and media files from development server
